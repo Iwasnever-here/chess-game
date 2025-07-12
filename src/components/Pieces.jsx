ba7fa255cfd2f7fd1c34296baf4631c1d0b3ba11
Lucy <lucycoe07@gmail.com>
2025-07-12 12:00:12 +0100

    making draggable pieces

--- src/components/Pieces.jsx
+++ src/components/Pieces.jsx
@@ -1,43 +1,47 @@
-import React from 'react'
 import Piece from './Piece'
+import {useState, useRef} from 'react'
+import { createPosition, copyPosition } from './helper'
 
 const Pieces = () => {
-    const position = new Array(8).fill('').map(x => new Array(8).fill(''))
 
-    for (let i=0; i<8; i++){
-        position[1][i] = 'wp'
-        position[6][i] = 'bp'
+    const ref = useRef()
+
+    const [state,setState] = useState(createPosition())
+
+    const calculateCoord = (e) => {
+      const {width,left,top} = ref.current.getBoundingClientRect()
+      const size = width/8
+      const y = Math.floor((e.clientX - left)/size)
+      const x = 7- Math.floor((e.clientY - top)/size)
+      return{x,y}
     }
-    position[0][0] = 'wr'
-    position[0][1] = 'wn'
-    position[0][2] = 'wb'
-    position[0][3] = 'wq'
-    position[0][4] = 'wk'
-    position[0][5] = 'wb'
-    position[0][6] = 'wn'
-    position[0][7] = 'wr'
 
-    position[7][0] = 'br'
-    position[7][1] = 'bn'
-    position[7][2] = 'bb'
-    position[7][3] = 'bq'
-    position[7][4] = 'bk'
-    position[7][5] = 'bb'
-    position[7][6] = 'bn'
-    position[7][7] = 'br'
+    const onDrop = (e) => {
+      const newPosition = copyPosition(state)
+      const {x,y} = calculateCoord(e)
+      const [p, rankStr, fileStr] = e.dataTransfer.getData('text').split(',')
 
-    console.log(position)
+      const rank = parseInt(rankStr)
+      const file = parseInt(fileStr)
+
+      newPosition[rank][file] = ''
+      newPosition[x][y] = p
+
+      setState(newPosition)
+    }
+
+    const onDragOver = (e) => e.preventDefault()
 
   return (
-    <div className='pieces'>
-      {position.map((r, rank) =>
+    <div className='pieces' onDrop = {onDrop} onDragOver = {onDragOver} ref = {ref} >
+      {state.map((r, rank) =>
     r.map((f,file) =>
-    position[rank][file] ?
+    state[rank][file] ?
     <Piece 
         key = {rank + '-' + file}
         rank = {rank}
         file = {file}
-        piece = {position[rank][file]}
+        piece = {state[rank][file]}
     /> :
     null))}
     </div>
